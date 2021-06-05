0ccaab67de5a09da4f65a79d0d78a82d881fe58d
Liron Wulffhart <liron.wulffhart@888holdings.com>
2021-06-05 14:38:50 +0300

    return casion .com home page as static file

--- Lambda-Demo/Lambda-Demo/Lambda-Demo/Startup.cs
+++ Lambda-Demo/Lambda-Demo/Lambda-Demo/Startup.cs
@@ -1,5 +1,8 @@
 using System;
+using System.IO;
 using System.Threading.Tasks;
+using Amazon.Lambda.AspNetCoreServer;
+using Amazon.Lambda.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -26,6 +29,7 @@ namespace Lambda_Demo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            /*
             app.Run(async context =>
             {
                 for (int i = 0; i < 10; ++i)
@@ -33,6 +37,23 @@ namespace Lambda_Demo
                     await Task.Delay(1000);
                     await context.Response.WriteAsync($"{i}{Environment.NewLine}");
                 }
+            });
+            */
+
+            app.Run(async context =>
+            {
+                context.Response.StatusCode = 200;
+                context.Response.ContentType = "text/html";
+
+                if (context.Items.TryGetValue(AbstractAspNetCoreFunction.LAMBDA_CONTEXT, out object lambdaContextObj) &&
+                    lambdaContextObj is ILambdaContext lambdaContext)
+                {
+                    context.Response.Headers["AwsRequestId"] = lambdaContext?.AwsRequestId;
+                }
+
+                await using var stream = File.OpenRead("casino-com.html");
+
+                await stream.CopyToAsync(context.Response.Body);
             });
         }
     }
